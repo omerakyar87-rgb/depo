3379a8baf0669f11c972eccb303b6b0a4b4de255
omerakyar87-rgb <omerakyar87@gmail.com>
2026-05-08 13:07:44 +0300

    Update from VSMobil (08.05.2026 13:07:08)

---
+++ welcome.js
@@ -0,0 +1,25 @@
+// VSMobil'e Hos Geldiniz!
+// Mobil cihazlarda kod yazmanin en kolay yolu
+
+// Ornek JavaScript kodu:
+function merhaba(isim) {
+  console.log(`Merhaba, ${isim}! VSMobil'e hos geldin.`);
+}
+
+// Fonksiyonu cagir
+merhaba('Gelistirici');
+
+// Matematiksel islemler
+const sayi1 = 10;
+const sayi2 = 20;
+console.log(`Toplam: ${sayi1 + sayi2}`);
+
+// Dizi islemleri
+const meyveler = ['Elma', 'Armut', 'Muz', 'Cilek'];
+console.log('Meyveler:', meyveler);
+
+meyveler.forEach((meyve, index) => {
+  console.log(`${index + 1}. ${meyve}`);
+});
+
+// Calistirmak icin sag ustteki "Calistir" butonuna tiklayin!
